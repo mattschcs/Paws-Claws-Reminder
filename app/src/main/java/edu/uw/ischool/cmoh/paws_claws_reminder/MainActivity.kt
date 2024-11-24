bca6bc7d16d823c6756767b7ce53a4f1d27a3f76
OliviaSapp2 <osapp2@uw.edu>
2024-11-24 14:56:14 -0800

    nav bar created to move between pages

--- app/src/main/java/edu/uw/ischool/cmoh/paws_claws_reminder/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/cmoh/paws_claws_reminder/MainActivity.kt
@@ -1,12 +1,20 @@
 package edu.uw.ischool.cmoh.paws_claws_reminder
 
+import android.app.Activity
 import android.os.Bundle
+import android.view.inputmethod.InputBinding
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import edu.uw.ischool.cmoh.paws_claws_reminder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavBar: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,29 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        bottomNavBar = findViewById(R.id.bottomNavigationView)
+        replaceFragment(Pets())
+
+        bottomNavBar.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.pets -> replaceFragment(Pets())
+                R.id.task -> replaceFragment(Task())
+                R.id.reminders -> replaceFragment(Reminders())
+                R.id.profile -> replaceFragment(Profile())
+
+                else -> {
+
+                }
+            }
+            true
+        }
+    }
+
+//    replace the frame layout with the fragments
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
